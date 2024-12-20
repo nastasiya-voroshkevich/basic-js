1d7aa95fe87edcece083b912f7114b135fa05082
Nastia <the_sun.89@mail.ru>
2024-12-20 20:09:59 +0300

    feat: add solutions for  dream-team, sum-digits, get-email-domain

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let str = [];
+  let el = [];
+  if (members) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === "string") {
+
+        str.push(members[i].split(' ').join('')[0]);
+      }
+
+    }
+  }
+  return str.join("").toUpperCase().split('').sort().join('');
 }
 
 module.exports = {
